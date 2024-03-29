83591d08bf1a148c2a97ef295f9df973e26618ea
test <test@debian-11-4.localdomain>
2024-03-29 16:40:01 +0530

    memory limit wordpress

--- download-extra.sh
+++ download-extra.sh
@@ -10,6 +10,7 @@ composer config --no-plugins allow-plugins.composer/installers true
 composer require humanmade/s3-uploads
 sed -i "/<?php/a \
 require_once __DIR__ . '/vendor/autoload.php';\\n\
+define('WP_MEMORY_LIMIT', '256M');\\n\
 define( 'S3_UPLOADS_BUCKET' , '$AMIT_S3_BUCKET/' );\\n\
 define( 'S3_UPLOADS_REGION', '$AMIT_S3_REGION' );\\n\
 define( 'S3_UPLOADS_USE_INSTANCE_PROFILE', true );\\n" /opt/bitnami/wordpress/wp-config.php
